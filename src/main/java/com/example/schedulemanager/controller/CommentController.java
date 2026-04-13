1cee4be85cc80b3c71d86a947cd2ef1e10914ae2
sighingpotato <jot0202@naver.com>
2026-04-13 17:28:08 +0900

    controller 작성 및 댓글 기능 완성

--- src/main/java/com/example/schedulemanager/controller/CommentController.java
+++ src/main/java/com/example/schedulemanager/controller/CommentController.java
@@ -1,4 +1,24 @@
 package com.example.schedulemanager.controller;
 
+import com.example.schedulemanager.dto.CommentRequestDto;
+import com.example.schedulemanager.dto.CommentResponseDto;
+import com.example.schedulemanager.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/schedules/{scheduleId}/comments")
 public class CommentController {
+    private final CommentService commentService;
+
+    @PostMapping
+    public ResponseEntity<CommentResponseDto> createComment(
+            @PathVariable Long scheduleId,
+            @RequestBody CommentRequestDto requestDto) {
+        return new ResponseEntity<>(commentService.createComment(scheduleId, requestDto), HttpStatus.CREATED);
+    }
 }
